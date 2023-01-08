d8e76f10deb543a6dfa742da532a9ca21fd00a14
Maxime Rochon-Michaud <maximerochonmichaud@outlook.com>
2023-01-07 20:31:59 -0500

    Update DAP config

--- nvim/lua/user/dap.lua
+++ nvim/lua/user/dap.lua
@@ -1,5 +1,11 @@
 local dap = require("dap")
 
+local dap_virtual_text = require("nvim-dap-virtual-text")
+dap_virtual_text.setup()
+
+local dap_python = require("dap-python")
+dap_python.setup()
+
 dap.adapters.python = {
     type = "executable",
     command = "/usr/local/bin/python3",
@@ -36,7 +42,11 @@ vim.keymap.set("n", "<leader>b", "<CMD>lua require'dap'.toggle_breakpoint()<CR>"
 vim.keymap.set("n", "<leader>br", "<CMD>lua require'dap'.repl.open()<CR>")
 vim.keymap.set("n", "<leader>B", "<CMD>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
 vim.keymap.set("n", "<F9>", "<CMD>lua require'dap'.continue()<CR>")
-vim.keymap.set("n", "<F10>", "<CMD>lua require'dap'.step_over()<CR>")
-vim.keymap.set("n", "<F11>", "<CMD>lua require'dap'.step_into()<CR>")
+vim.keymap.set("n", "<F10>", "<CMD>lua require'dap'.step_into()<CR>")
+vim.keymap.set("n", "<F11>", "<CMD>lua require'dap'.step_over()<CR>")
 vim.keymap.set("n", "<F12>", "<CMD>lua require'dap'.step_out()<CR>")
+
+vim.keymap.set("n", "<leader>dm", "<CMD>lua require'dap-python'.test_method()<CR>")
+vim.keymap.set("n", "<leader>dc", "<CMD>lua require'dap-python'.test_class()<CR>")
+vim.keymap.set("n", "<leader>ds", "<CMD>lua require'dap-python'.debug_selection()<CR>")
 
